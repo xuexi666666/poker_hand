3dd072ad9ad55cc379c6d4e19181c03e81d0ba2e
xuexi666666 <235400554@qq.com>
2019-08-11 22:07:48 +0800

    Determine if the poker is a pair of subtypes

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -6,6 +7,19 @@ import java.util.Arrays;
 
 public class PokerTest {
     private ArrayList<Card> pairCards;
+    private Poker onePair;
+
+    @Before
+    public void init(){
+        //given
+        pairCards = new ArrayList<>();
+        pairCards.add(new Card("3", "C"));
+        pairCards.add(new Card("3", "S"));
+        pairCards.add(new Card("5", "H"));
+        pairCards.add(new Card("6", "S"));
+        pairCards.add(new Card("T", "D"));
+        onePair = new Poker(pairCards);
+    }
     @Test
     public void should_return_who_win_when_give_each_player_one_poker(){
         Card card1 = new Card("3","H");
@@ -16,4 +30,11 @@ public class PokerTest {
         Assert.assertEquals(GameProperty.POKER_2_WINNER,res);
     }
 
+    @Test
+    public void should_return_ONE_PAIR_win_when_give_a_pair_of_identical_cards(){
+        //when
+        int res = onePair.isOnePair();
+        //then
+        Assert.assertEquals(res,GameProperty.ONE_PAIR);
+    }
 }
